db88bab916b1ea908679f30165dba8cd8ae44b30
anthonydevelops <anthonydevelops@gmail.com>
2018-10-19 13:32:31 -0700

    Updated JSON for all of BSOE

--- api/soe.js
+++ api/soe.js
@@ -5,10 +5,9 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
-const classes = require("./data/courses.json");
-const branches = require("./data/departments.json");
-const general = require("./data/ge.json");
-const schedules = require("./data/schedule.json");
+const path = require("path");
+
+const dirPath = path.join(__dirname, "/data/depo");
 
 /* Use of scraper documentation can be found here:
  * https://www.npmjs.com/package/cheerio
@@ -89,85 +88,128 @@ const getCourses = () => {
  * go to ./data to check schedule.json
  */
 const getSchedule = () => {
-  axios.get("https://courses.soe.ucsc.edu/courses/cmps").then(res => {
-    if (res.status === 200) {
-      const html = res.data;
-      const $ = cheerio.load(html);
-      let schedule = [];
-      $(".course-name").map((i, elem) => {
-        // Get course ID
-        const courses = $(elem)
-          .children("a")
-          .text()
-          .split(":")
-          .map(tag => tag.trim());
+  for (let i = 0; i < branches.length; i++) {
+    axios.get(branches[i].url).then(res => {
+      if (res.status === 200) {
+        const html = res.data;
+        const $ = cheerio.load(html);
+        let schedule = [];
+        $(".course-name").map((i, elem) => {
+          // Get course ID
+          const courses = $(elem)
+            .children("a")
+            .text()
+            .split(":")
+            .map(tag => tag.trim());
 
-        // Get quarters offered
-        let season = $(elem)
-          .parent()
-          .next()
-          .children(".class")
-          .find("li")
-          .children("a")
-          .map((i, elem) => {
-            return $(elem).attr("href");
-          })
-          .get();
+          // Get quarters offered
+          let season = $(elem)
+            .parent()
+            .next()
+            .children(".class")
+            .find("li")
+            .children("a")
+            .map((i, elem) => {
+              return $(elem).attr("href");
+            })
+            .get();
 
-        // Break apart the href into an array
-        season = season.map((i, elem) => {
-          return i
-            .split("/")
+          // Break apart the href into an array
+          season = season.map((i, elem) => {
+            return i
+              .split("/")
+              .map(tag => tag.trim())
+              .filter(function(n) {
+                return n != "";
+              });
+          });
+
+          // Get section data of course
+          const section = $(elem)
+            .parent()
+            .next()
+            .children(".class")
+            .find("li")
+            .contents()
+            .text()
+            .split("\n")
             .map(tag => tag.trim())
             .filter(function(n) {
               return n != "";
             });
+
+          // If the course is offered, return data
+          if (season.length > 0 && section.length > 0) {
+            const available = season.map((i, el) => {
+              return i[2];
+            });
+
+            schedule[i] = {
+              courseID: courses[0],
+              quarters: [...new Set(available)],
+              sections: section
+            };
+          }
+          // If the course is not offered, return blank data
+          else {
+            schedule[i] = {
+              courseID: courses[0],
+              quarters: [],
+              sections: []
+            };
+          }
         });
+        // Send data to a file in JSON
+        fs.writeFile(
+          `data/depo/${branches[i].depoID}.json`,
+          JSON.stringify(schedule, null, 4),
+          err => {
+            console.log("File successfully written");
+          }
+        );
+      }
+    });
+  }
+};
 
-        // Get section data of course
-        const section = $(elem)
-          .parent()
-          .next()
-          .children(".class")
-          .find("li")
-          .contents()
-          .text()
-          .split("\n")
-          .map(tag => tag.trim())
-          .filter(function(n) {
-            return n != "";
-          });
-
-        // If the course is offered, return data
-        if (season.length > 0 && section.length > 0) {
-          const available = season.map((i, el) => {
-            return i[2];
-          });
-
-          schedule[i] = {
-            courseID: courses[0],
-            quarters: [...new Set(available)],
-            sections: section
-          };
-        }
-        // If the course is not offered, return blank data
-        else {
-          schedule[i] = {
-            courseID: courses[0],
-            quarters: [],
-            sections: []
-          };
-        }
-      });
-      // Send data to a file in JSON
-      fs.writeFile(
-        "data/schedule.json",
-        JSON.stringify(schedule, null, 4),
-        err => {
-          console.log("File successfully written");
-        }
-      );
+/* Merge all course data into one JSON file
+ * DON'T CALL UNLESS FRESH JSON IS NEEDED
+ * go to /api/data to check out the new schedule.json
+ */
+const mergeData = () => {
+  const allSchedules = [];
+  let curr = 0;
+  // Read directory based off directory path
+  fs.readdir(dirPath, (err, files) => {
+    if (err) {
+      return console.log(`Unable to scan directory ${err}`);
     }
+    // Loop through each file, require it, then merge
+    files.forEach(file => {
+      const data = require(`./data/depo/${file}`);
+      for (let i = 0; i < classes.length; i++) {
+        if (data[curr] !== undefined) {
+          if (classes[i].courseID === data[curr].courseID) {
+            allSchedules.push({
+              courseID: classes[i].courseID,
+              courseTitle: classes[i].courseTitle.trim(),
+              terms: data[curr].quarters,
+              sections: data[curr].sections
+            });
+            curr++;
+          }
+        }
+      }
+      curr = 0;
+    });
+    // Write file to JSON
+    fs.writeFile(
+      "data/schedule.json",
+      JSON.stringify(allSchedules, null, 4),
+      err => {
+        console.log("File successfully written");
+      }
+    );
   });
 };
 
@@ -220,9 +262,7 @@ const checkSchedule = id => {
 };
 
 module.exports = {
-  getDepartments,
-  getCourses,
-  getSchedule,
+  mergeData,
   checkGE,
   checkDepartment,
   checkCourse,
