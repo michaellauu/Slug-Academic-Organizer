c9ae7aed831133ca501716b8612f1109a0f4f73f
Michael <michaellauu@gmail.com>
2018-10-19 00:44:43 -0700

    ttt

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom'
+import Home from './Home';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const scraper = require("./api/soe.js");
 var bodyParser = require("body-parser");
-
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+// Set up Mongoose
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds137003.mlab.com:37003/re');
+mongoose.Promise = global.Promise;
+
+
+// API routes
+require('./server/routes/')(app);
+
 
 app.use(
   bodyParser.urlencoded({
