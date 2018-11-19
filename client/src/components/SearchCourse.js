294d1fb747772a80307d1ca831c9a3728e9d390d
chtzou <chtzou@ucsc.edu>
2018-11-18 20:51:44 -0800

    temp SearchCourse

--- client/src/components/SearchCourse.js
+++ client/src/components/SearchCourse.js
@@ -14,6 +14,74 @@ import {
 import 'instantsearch.css/themes/reset.css';
 import './SearchCourse.css';
 import { CollapsibleComponent, CollapsibleHead, CollapsibleContent } from 'react-collapsible-component'
+import algoliasearch from 'algoliasearch';
+import mongoose from 'mongoose';
+import mongoolia from 'mongoolia';
+
+// Pass `{algoliaIndex: true}` to push theses attributes for indexing to Algolia
+const newIndex = new mongoose.Schema({
+  description: { type: String, required: true, algoliaIndex: true },
+  prereqs: { type: String, required: true, algoliaIndex: true },
+  lecture: {
+    days: { type: String, required: true, algoliaIndex: true },
+    times: { type: String, required: true, algoliaIndex: true },
+    room: { type: String, required: true, algoliaIndex: true },
+    instructor: { type: String, required: true, algoliaIndex: true },
+    meetingDates: { type: String, required: true, algoliaIndex: true }
+  },
+  profReview: { 
+    rating: { type: String, required: true, algoliaIndex: true },
+    amountReviewed: { type: String, required: true, algoliaIndex: true }
+  },
+  sections: { type: Array, required: true, algoliaIndex: true },
+  courseTitle: { type: String, required: true, algoliaIndex: true },
+  courseID: { type: String, required: true, algoliaIndex: true },
+  meta: { 
+    career: { type: String, required: true, algoliaIndex: true },
+    grading: { type: String, required: true, algoliaIndex: true },
+    class_number: { type: String, required: true, algoliaIndex: true },
+    type: { type: String, required: true, algoliaIndex: true },
+    credits: { type: String, required: true, algoliaIndex: true },
+    general_education: { type: String, required: true, algoliaIndex: true },
+    status: { type: String, required: true, algoliaIndex: true },
+    available_seats: { type: String, required: true, algoliaIndex: true },
+    enrollment_capacity: { type: String, required: true, algoliaIndex: true },
+    enrolled: { type: String, required: true, algoliaIndex: true },
+    wait_list_capacity: { type: String, required: true, algoliaIndex: true }
+   },
+});
+
+// Specify your Algolia credentials which you can find into your dashboard
+newIndex.plugin(mongoolia, {
+    apiKey:"e0f4e3542265d408d5be8de22d00a12e",
+    appId:"Z4ZULV1BRS",
+    indexName:"fall18"
+})
+const course = mongoose.model("course", newIndex)
+course.syncWithAlgolia();
+
+/*
+const chunk = require('lodash.chunk')
+const client = algoliasearch('Z4ZULV1BRS', '1b0bac00d91c1064ed69c2b075bdc690');
+const index = client.initIndex('fall18');
+function fetchDataFromDatabase() {
+    const fall18 = new mongoose.Schema({
+        description: { type: String, required: true, algoliaIndex: true },
+        prereqs: { type: String, required: true, algoliaIndex: true },
+        lecture: { type: Object, required: true, algoliaIndex: true },
+        sections: { type: Array, required: true, algoliaIndex: true },
+        courseTitle: { type: String, required: true, algoliaIndex: true },
+        courseID: { type: String, required: true, algoliaIndex: true },
+        meta: { type: Object, required: true, algoliaIndex: true },
+      });
+    return fall18;
+}
+const records = fetchDataFromDatabase();
+const chunks = chunk(records, 1000);
+chunks.map(function(batch) {
+  return index.addObjects(batch);
+});
+*/
 
 /*
 *Used to display course information (course Title, description, instructor, days, times, room)
@@ -26,7 +94,7 @@ function Course({ hit }) {
                 <Highlight attribute="courseTitle" hit={hit} tagName="mark" />
                 <p><Highlight attribute="description" hit={hit} tagName="mark" /></p>
                 <p>
-                    Instrutor: <Highlight attribute="lecture.Instructor" hit={hit} tagName="mark" />
+                    Instructor: <Highlight attribute="lecture.Instructor" hit={hit} tagName="mark" />
                     <br />Days and Times: <Highlight attribute="lecture.DaysTimes" hit={hit} tagName="mark" />
                     <br />Room: <Highlight attribute="lecture.Room" hit={hit} tagName="mark" />
                 </p>
